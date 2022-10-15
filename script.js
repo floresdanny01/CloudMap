fd6cb9c00ea00c4c53b868cc58601db19748d19b
floresdanny01 <42793001+floresdanny01@users.noreply.github.com>
2022-10-15 15:44:24 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "40c13ed31879d9c7",
+      center: { lat: 33.87, lng: -117.86 },
+      zoom: 12,
+    });
+  }
+  
