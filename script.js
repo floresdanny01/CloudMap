77146e53e46709bb0796b3daa6ceb604fc6df418
cerick826 <cerick652@gmail.com>
2022-11-19 01:43:20 -0800

    Added login page and new functions

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 function initMap() {
-    new google.maps.Map(document.getElementById("map"), {
-      mapId: "40c13ed31879d9c7",
-      center: { lat: 33.87, lng: -117.86 },
-      zoom: 12,
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "40c13ed31879d9c7",
+    center: { lat: 33.87, lng: -117.86 },
+    zoom: 12,
+  });
+
+  /* Setting up searchbox autocomplete features*/
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('searchbar'),
+    {
+      types: ['establishment'],
+      compnentRestrictions: { 'country': ['US'] },
+      fields: ['place_id', 'geometry', 'name']
     });
 
-    /* Setting up searchbox autocomplete features*/
-    autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('searchbar'),
-      {
-        types: ['establishment'],
-        compnentRestrictions: {'country': ['US']},
-        fields: ['place_id', 'geometry', 'name']
-      });
-    
-    autocomplete.addlistener('place_changed', onPlaceChanged);
+  autocomplete.addlistener('place_changed', onPlaceChanged);
 
-    function onPlaceChanged(){
-      var place = autocomplete.getPlace();
-      if (!place.geometry){
-        // Users search did not return a place; reset the searchbar 
-        document.getElementById('autocomplete').placeholder =
+  function onPlaceChanged() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // Users search did not return a place; reset the searchbar 
+      document.getElementById('autocomplete').placeholder =
         'Enter a place';
-      } else {
-        // Display the details of the place searched
-        document.getElementById('details').innerHTML = place.name;
-      }
+    } else {
+      // Display the details of the place searched
+      document.getElementById('details').innerHTML = place.name;
     }
-
   }
+
+}
+
 
