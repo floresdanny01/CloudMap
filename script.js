57f9f1b3e699af92aa157722a5cf95ee28ffd2de
charliegale16 <charlesgale@csu.fullerton.edu>
2022-12-05 19:00:06 -0800

    heatmap update, key change

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
+  
+  var heatmapData =  [ {location: new google.maps.LatLng(33.882993, -117.885081), weight:(135161/30000)},
+  {location: new google.maps.LatLng(33.836111111111116, -117.88972222222223), weight:(336000/30000)},
+  {location: new google.maps.LatLng(33.86666666666667, -117.56666666666666), weight:(158391/30000)},
+  {location: new google.maps.LatLng(33.92333333333333, -117.8888888888889), weight:(39282/30000)},
+  {location: new google.maps.LatLng(33.665, -117.91222222222223), weight:(109960/30000)},
+  {location: new google.maps.LatLng(33.85611111111111, -118.00416666666666), weight:(80530/30000)},
+  {location: new google.maps.LatLng(33.80305555555555, -117.8325), weight:(136416/30000)},
+  {location: new google.maps.LatLng(33.740833333333335, -117.88138888888888), weight:(324528/30000)},
+  {location: new google.maps.LatLng(33.68416666666666, -117.7925), weight:(215529/30000)},
+  {location: new google.maps.LatLng(33.69277777777778, -118.00027777777778), weight:(197575/30000)},
+  
+  ];
+
+
+
+  var map = new google.maps.Map(document.getElementById("map"), {
     mapId: "40c13ed31879d9c7",
     center: { lat: 33.87, lng: -117.86 },
     zoom: 12,
   });
+  
+  var heatmap = new google.maps.visualization.HeatmapLayer({
+    data : heatmapData,
+    map: map,
+    radius: 70,
+    dissipating: true
+  });
 
+ 
+  document.getElementById("toggle-heatmap")
+  .addEventListener("click", toggleHeatmap);
+
+  function toggleHeatmap() {
+    heatmap.setMap(heatmap.getMap() ? null : map);
+  }
+  
   /* Setting up searchbox autocomplete features*/
   autocomplete = new google.maps.places.Autocomplete(
     document.getElementById('searchbar'),
@@ -27,7 +58,8 @@ function initMap() {
       document.getElementById('details').innerHTML = place.name;
     }
   }
+  
+
+  
 
 }
-
-
